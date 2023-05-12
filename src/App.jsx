38980be9169719dea9f558c4ffc37ab817b0990d
imranwebdev <imranwebdevbd2022@gmail.com>
2023-05-12 14:54:24 +0600

    this is the last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-import './App.css'
 
+import FilterbleProductTable from './components/filterableProductTable'
 function App() {
-  const [count, setCount] = useState(0)
+  const PRODUCTS = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  ]
 
   return (
     <>
-
+<FilterbleProductTable products = {PRODUCTS}/>
     </>
   )
 }
